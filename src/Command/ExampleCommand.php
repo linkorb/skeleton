7bc4cbb941a6aaf8af8c12775cf3dc53c77407aa
Hongliang <h.wang@linkorb.com>
2015-06-02 10:03:37 +0200

    initial checkout

---
+++ src/Command/ExampleCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace LinkORBSkeleton\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use InvalidArgumentException;
+
+class ExampleCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('skeleton:example')
+            ->setDescription('An example command');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<info>Cool!</info>');
+    }
+}
---
+++ src/Controller/ExampleController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Herald\Server\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ExampleController
+{
+    public function indexAction(Application $app, Request $request)
+    {
+        return new Response('Cool');
+    }
+}
